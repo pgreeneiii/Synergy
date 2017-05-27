d925597dc2a6ca9686abb02c16bee9b9c6f6c082
Phil Greene <pgreeneiii@users.noreply.github.com>
2017-05-27 17:59:24 -0500

    Tons of Mailboxer Stuff loaded up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+   protect_from_forgery with: :exception
+   before_action :configure_permitted_parameters, if: :devise_controller?
+
+   protected
+
+   def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, :keys => [:name])
+      devise_parameter_sanitizer.permit(:account_update, :keys => [:name])
+   end
+
+   rescue_from ActiveRecord::RecordNotFound do
+      flash[:warning] = 'Resource not found.'
+      redirect_back_or root_path
+   end
+
+   def redirect_back_or(path)
+      redirect_to request.referer || path
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: "home#index"
+
+  resources :conversations, only: [:index, :show, :destroy]
+
+  get("/sign-up", {:controller => "devise", :action => "new" })
+
 end
